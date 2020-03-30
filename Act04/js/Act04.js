5aa0b09c6ec6915c91869920f813cff28c42efef
Averdusco <averdusco@institutmvm.cat>
2020-03-30 12:02:20 +0200

    M08-UF2-Act04

---
+++ Act04/js/Act04.js
@@ -0,0 +1,32 @@
+var primera = 10;
+var segunda = 20;
+var tercera = 30;
+var quarta = 40;
+
+if (quarta >= 40) {
+  tasca1.innerHTML += 'La quarta es la variable major';
+} else {
+  tasca1.innerHTML += 'Es fals';
+}
+
+var primera = 30;
+var segunda = 20;
+var tercera = 10;
+var quarta = 40;
+
+if (tercera <= 10) {
+  tasca2.innerHTML += 'La tercera es la variable més petita';
+} else {
+  tasca2.innerHTML += 'Es fals';
+}
+
+var primera = 10;
+var segunda = 20;
+var tercera = 30;
+var quarta = 40;
+
+if (primera == segunda == tercera == quarta) {
+  tasca3.innerHTML += 'Són iguals'
+} else {
+  tasca3.innerHTML += 'No hi ha cap variable que siguin iguals'
+}
